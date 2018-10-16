d67d8286d6f6427f8ed62b3d51946695b41dc5ed
Katrina Pritchard <pritckat@gmail.com>
2018-10-16 01:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const token = 'd058b2b3a7db001a6ea1868d2cd19191202b94a5';
+const baseURL = 'https://api.github.com';
+const user = 'pritckat';
 
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
-  .then(res => res.json())
-  .then(json => console.log(json));
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
@@ -16,10 +10,23 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showResults(json)); 
+  }
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
+    json.html_url
+  }</a>`;
 }
 
 function createIssue() {
